a1cfcd9a7329d09f4f31aea86434badc790a8270
OddMagnet <michael@bruenen.cloud>
2026-04-14 16:51:22 +0200

    Make backspace command configurable via autopair_backspace_command
    Same pattern as autopair_complete_command for Tab. If set to a valid
    function name, _autopair_backspace calls that instead of the default
    backward-delete-char. This allows composing autopair with plugins
    that override backspace (e.g. natural-selection).

--- functions/_autopair_backspace.fish
+++ functions/_autopair_backspace.fish
@@ -5,5 +5,9 @@ function _autopair_backspace
     test $index -ge 1 &&
         contains -- (string sub --start=$index --length=2 -- "$buffer") $autopair_pairs &&
         commandline --function delete-char
-    commandline --function backward-delete-char
+    if set --query autopair_backspace_command[1]; and functions --query $autopair_backspace_command
+        $autopair_backspace_command
+    else
+        commandline --function backward-delete-char
+    end
 end
