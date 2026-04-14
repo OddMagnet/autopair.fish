d0452e93dcbd15298630d98435089fc05ea8768c
Michael Brünen <34708235+OddMagnet@users.noreply.github.com>
2026-04-14 18:53:13 +0200

    Update functions/_autopair_tab.fish
    Check if first argument is an existing function instead of all arguments
    
    Co-authored-by: justbispo <38961424+justbispo@users.noreply.github.com>

--- functions/_autopair_tab.fish
+++ functions/_autopair_tab.fish
@@ -3,7 +3,7 @@ function _autopair_tab
 
     string match --quiet --regex -- '\$[^\s]*"$' (commandline --current-token) &&
         commandline --function end-of-line --function backward-delete-char
-    if set --query autopair_complete_command[1]; and functions --query $autopair_complete_command
+    if set --query autopair_complete_command[1]; and functions --query $autopair_complete_command[1]
         $autopair_complete_command
     else
         commandline --function complete
