dec5ca88c30b603f3137be0f297933f9e55e0d77
Michael Brünen <34708235+OddMagnet@users.noreply.github.com>
2026-04-14 18:53:03 +0200

    Update functions/_autopair_backspace.fish
    Check if first argument is an existing function instead of all arguments
    
    Co-authored-by: justbispo <38961424+justbispo@users.noreply.github.com>

--- functions/_autopair_backspace.fish
+++ functions/_autopair_backspace.fish
@@ -5,7 +5,7 @@ function _autopair_backspace
     test $index -ge 1 &&
         contains -- (string sub --start=$index --length=2 -- "$buffer") $autopair_pairs &&
         commandline --function delete-char
-    if set --query autopair_backspace_command[1]; and functions --query $autopair_backspace_command
+    if set --query autopair_backspace_command[1]; and functions --query $autopair_backspace_command[1]
         $autopair_backspace_command
     else
         commandline --function backward-delete-char
